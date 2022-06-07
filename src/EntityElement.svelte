a4aee7bea3fea802719a9881fdd90461efe6c5f1
wojciechsmigielskijr <43293521+wojciechsmigielskijr@users.noreply.github.com>
2022-06-08 00:16:28 +0100

    Changes
    Changes

---
+++ src/EntityElement.svelte
@@ -0,0 +1,30 @@
+<script>
+    let EntityExists = false;
+
+    function AddSubEntityBox(elem) {
+        var inputEntity = document.createElement("input");
+        inputEntity.placeholder = "Enter the Sub Entity."
+        inputEntity.className = "LuisSubEntity";
+        var inputEntityText = document.createElement("input");
+        inputEntityText.placeholder = "Enter the text that features the Sub entity."
+        inputEntityText.className = "SubEntityText";
+        var ptag = document.createElement("p");
+
+        var element = elem.srcElement.parentElement;
+
+        console.log(element);
+
+        element.append(ptag)
+        element.append(inputEntity);
+        element.append(inputEntityText);
+
+    }
+</script>
+
+<p class="Entity">
+    <p> <b>Entity</b> </p>
+    <input class="LuisEntity" placeholder="Enter Your Entity">
+    <input class="EntityText" placeholder="Enter Your the text of the entity">
+    <p class="SubEntity">
+        <button on:click={AddSubEntityBox} type="button">Add Sub Entity</button>
+    </p>     
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,34 @@
+<script>
+import EntityElement from "/src/EntityElement.svelte";
+	let UtteranceAmount = 0;
+
+    function AddUtterance() {
+        UtteranceAmount += 1;
+    }
+
+    function AddEntityBox() {
+        const element = new EntityElement({
+            target: document.getElementById("Entities")
+        })
+    }
+
+</script>
+
 <h1>Luis Batch Test Generator</h1>
+
+<form width="100%">
+    <input id="utterance" placeholder="Enter your utterance.">
+    <input id="intent" placeholder="Enter your intent.">
+
+    <p id="Entities">
+        <button on:click={AddEntityBox} type="button" id="EntityButton">Add Entity</button>
+        <entity></entity>
+    </p>   
+</form>
+
+<p>
+    <button on:click={AddUtterance}>Add Record</button>
+</p>
+
+<p>Amount of Utterance added: {UtteranceAmount}</p>
+<button>Download JSON</button>
